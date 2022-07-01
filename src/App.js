adb2172b3fe3fc17c218c5a5998282e404f2f45d
Mark McConnell <amark.mcconnell@gmail.com>
2022-07-01 09:23:35 -0700

    update add.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import Header from './components/Header';
+import KegControl from './components/KegControl';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <KegControl />
     </React.Fragment>
   );
 }
